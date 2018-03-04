88ecf62972e752ae6828649ccde6e40b4688a4af
tanyaw <twanwatanakool@gmail.com>
2018-03-03 17:16:33 -0800

    Derek's confused.

--- snackDB.py
+++ snackDB.py
@@ -4,9 +4,9 @@
 import sqlite3
 connection=sqlite3.connect("snackOverFlow.db")   #name of file where data is stored, and is name of db
 cursor=connection.cursor()
-cursor.execute("""DROP TABLE product""")        #for each table
-cursor.execute("""DROP TABLE cart""")
-cursor.execute("""DROP TABLE member""")
+#cursor.execute("""DROP TABLE product""")        #for each table
+#cursor.execute("""DROP TABLE cart""")
+#cursor.execute("""DROP TABLE member""")
 ################################################################################     start creating tables 
 #creating product table using python to sql
 #table for product 
@@ -32,7 +32,7 @@ CREATE TABLE member (
 memberID INTEGER PRIMARY KEY,
 name VARCHAR(20),  
 email VARCHAR(30),
-password PIN, 
+password VARCHAR(4), 
 berf DOB,
 address VARCHAR(40));"""
 cursor.execute(sql_command)    #end 
@@ -52,39 +52,39 @@ cursor.execute(sql_command)    #end
 ######################################################################### end inserting data 
 
 
-# sql_command="""INSERT INTO product (bar_code, price, name, description, stock)
-#         VALUES (NULL, 5.32, "egg", "nice and yokey",30);"""
-# cursor.execute(sql_command)
+sql_command="""INSERT INTO product (bar_code, price, name, description, stock)
+        VALUES (NULL, 5.32, "egg", "nice and yokey",30);"""
+cursor.execute(sql_command)
 
-# sql_command="""INSERT INTO product (bar_code, price, name, description, stock)
-#         VALUES (NULL, 5.32, "bacon", "bacony",30);"""
-# cursor.execute(sql_command)
+sql_command="""INSERT INTO product (bar_code, price, name, description, stock)
+        VALUES (NULL, 5.32, "bacon", "bacony",30);"""
+cursor.execute(sql_command)
 
-# sql_command="""INSERT INTO product (bar_code, price, name, description, stock)
-#         VALUES (NULL, 5.32, "cheese", "cheesey",30);"""
-# cursor.execute(sql_command)
-
-
-# sql_command="""INSERT INTO member (memberID,name,email,password,berf,address)
-#             VALUES (0,"Tanya","tanya@cpp.edu", 0000, "01/02/03",111);"""
-# cursor.execute(sql_command) 
-
-# sql_command="""INSERT INTO member (memberID,name,email,password,berf,address)
-#             VALUES (1,"Tanya2","tanya1@cpp.edu", 0001, "02/02/03",113);"""
-# cursor.execute(sql_command)
-# sql_command="""INSERT INTO member (memberID,name,email,password,berf,address)
-#             VALUES (2,"Tanya3","tanya13@cpp.edu", 0002, "03/02/03",114);"""
-# cursor.execute(sql_command)
+sql_command="""INSERT INTO product (bar_code, price, name, description, stock)
+        VALUES (NULL, 5.32, "cheese", "cheesey",30);"""
+cursor.execute(sql_command)
 
 
-# connection.commit()           #do this to save changes
+sql_command="""INSERT INTO member (memberID,name,email,password,berf,address)
+            VALUES (0,"Tanya","tanya@cpp.edu", "0000", "01/02/03",111);"""
+cursor.execute(sql_command) 
 
-# #GETTING DATA FROM TABLE SPECIFIED 
-# cursor.execute("SELECT * FROM product")
-# print("fetchall: ")
-# result= cursor.fetchall()
-# for x in result:
-#     print(x)
+sql_command="""INSERT INTO member (memberID,name,email,password,berf,address)
+            VALUES (1,"Tanya2","tanya1@cpp.edu", "0001", "02/02/03",113);"""
+cursor.execute(sql_command)
+sql_command="""INSERT INTO member (memberID,name,email,password,berf,address)
+            VALUES (2,"Tanya3","tanya13@cpp.edu", "0002", "03/02/03",114);"""
+cursor.execute(sql_command)
+
+
+connection.commit()           #do this to save changes
+
+#GETTING DATA FROM TABLE SPECIFIED 
+cursor.execute("SELECT * FROM product")
+print("fetchall: ")
+result= cursor.fetchall()
+for x in result:
+    print(x)
 
 
 connection.close()
